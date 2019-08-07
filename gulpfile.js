8a7cc1482bee2298545d93909c611658858280e0
Samuel <1224042766@qq.com>
2019-08-07 10:51:56 +0800

    Delete gulpfile.js
    删除gulpfile.js

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-gulp.task('default', function(){
-    return gulp.src('./css/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./css'))
-});
